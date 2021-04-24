455bacde60a671b258674dc5b94f6a40f8b733fd
Thiago Cavalcante <thiago.kun@gmail.com>
2021-04-24 02:19:39 -0300

    Chapter 1

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,13 +1,31 @@
 package tacos;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 @SpringBootTest
+@WebMvcTest(HomeController.class)
 class TacoCloudApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+		       .andExpect(status().isOk())
+		       .andExpect(view().name("home"))
+			   .andExpect((ResultMatcher) content().string(containsString("Welcome to...")));
 	}
 
 }
